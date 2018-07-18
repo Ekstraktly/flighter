e4b618f8c59698e3fa61ddc83932ba6d9deb7f1e
Josip Mesaric <josip.mesaric@hotmail.com>
2018-07-18 17:26:14 +0200

    Final fix

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
-  has_many :flights
+  has_many :flights, dependent: :nullify
   validates :name, uniqueness: { case_sensitive: false },
                    length: { maximum: 45 },
                    presence: true
