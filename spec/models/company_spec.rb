b71438f354b8e1723b9471c1f924152640aafe19
Josip Mesaric <josip.mesaric@hotmail.com>
2018-07-18 17:20:00 +0200

    Final fix

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,8 +1,10 @@
 RSpec.describe Company do
   describe 'uniqueness' do
-    subject { Company.new(name: 'Lufthansa') }
+    subject { described_class.new(name: 'Lufthansa') }
+
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   end
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to have_db_index(:name).unique(true) }
 end
