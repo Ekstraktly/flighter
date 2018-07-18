08ee6988587adfa54235031af937f65668b64818
Josip Mesaric <josip.mesaric@hotmail.com>
2018-07-18 14:31:21 +0200

    Fixv3

--- db/migrate/20180717154740_create_companies.rb
+++ db/migrate/20180717154740_create_companies.rb
@@ -1,7 +1,8 @@
 class CreateCompanies < ActiveRecord::Migration[5.2]
   def change
     create_table :companies do |t|
-      t.string :name, null: false, unique: true, index: true
+      t.index :name
+      t.string :name, null: false, unique: true
       t.timestamps null: false
     end
   end
