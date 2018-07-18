d238f31ec02b96fc12e1a28cd42e7869cb84c403
Josip Mesaric <josip.mesaric@hotmail.com>
2018-07-18 14:20:24 +0200

    Fixv1

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  belongs_to :users
-  belongs_to :flights
+  belongs_to :user
+  belongs_to :flight
   validates :seat_price, presence: true,
                          numericality: { greater_than: 0 }
   validates :no_of_seats, presence: true,
