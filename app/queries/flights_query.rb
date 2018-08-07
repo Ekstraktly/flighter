6a90efbd9bbd787a3e1fc8bc4f3b72280c3846ac
Josip Mesaric <josip.mesaric@hotmail.com>
2018-08-07 09:33:51 +0200

    Query fix

--- app/queries/flights_query.rb
+++ app/queries/flights_query.rb
@@ -12,4 +12,12 @@ class FlightsQuery
   def no_of_booked_seats
     relation.sum('bookings.no_of_seats')
   end
+
+  def with_stats
+    relation.joins(:flight)
+            .select('SUM(no_of_seats) AS total_no_of_booked_seats')
+            .select('SUM(no_of_seats*bookings.no_of_seats) AS total_revenue')
+            .select('SUM(booking.no_of_seats) / SUM(no_of_seats)
+                     AS occupancy')
+  end
 end
