ecb2b140f09e06cff2127e10a24c068a07d33379
Josip Mesaric <josip.mesaric@hotmail.com>
2018-08-08 12:56:52 +0200

    FlightCalculator fix

--- app/calculators/flight_calculator.rb
+++ app/calculators/flight_calculator.rb
@@ -1,5 +1,5 @@
 class FlightCalculator
-  attr_reader :flight
+  attr_reader :flight, :base_price, :flys_at
 
   def initialize(flight)
     @flight = flight
@@ -8,15 +8,19 @@ class FlightCalculator
   end
 
   def price
-    return @base_price * 2 if @flys_at <= Time.zone.now
-
-    @base_price + (((15 - days_to_flight) / 15.0) *
-      @base_price).to_i
+    return @base_price * 2 if flys_at <= Time.zone.now
+    return @base_price if flys_at > 15.days.from_now
+    calculate_price
   end
 
   private
 
   def days_to_flight
-    (@flys_at.to_date - Date.current).to_i
+    (flys_at.to_date - Date.current).to_i
+  end
+
+  def calculate_price
+    base_price + (((15 - days_to_flight) / 15.0) *
+      base_price).to_i
   end
 end
